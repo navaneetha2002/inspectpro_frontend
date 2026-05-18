4a16bd5b75a497a07fdc10265902ba2f0db48083
Vivek11N <viveknarayanan117@gmail.com>
2026-05-18 11:19:40 +0530

    feat :fixes

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://inspectpro-backend.cfapps.eu10-004.hana.ondemand.com',
         changeOrigin: true,
       }
     }
